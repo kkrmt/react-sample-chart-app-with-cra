a0e035261803f060f96e8504f13c479fba5c6e33
Kei Kurimoto <kurimoto.kei.721@gmail.com>
2021-05-05 20:41:28 +0900

    add react router and so on

--- src/App.tsx
+++ src/App.tsx
@@ -17,20 +17,8 @@ const App: FC<Props> = ({name}) => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>{fileName}</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>HPです</p>
+      
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -6,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name={"testのfileですよ"}/>
+    <BrowserRouter>
+      <App name={"testのfileですよ"}/>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
